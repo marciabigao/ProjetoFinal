b36d204ed5369ec07f397fbad905d586477899a0
Lara Amelia <laraameliamf@gmail.com>
2024-12-25 11:41:14 -0300

    Alteração de Jogador.hpp, com inclusão das assinaturas para os métodos le_estatisticas e atualiza_estatisticas

--- include/Jogador.hpp
+++ include/Jogador.hpp
@@ -8,17 +8,23 @@ class Jogador
     private:
     std::string nome_jogador;
     std::string apelido_jogador;
+
     int vitorias_reversi, derrotas_reversi;
     int vitorias_lig, derrotas_lig;
     int vitorias_jvelha, derrotas_jvelha;
+    
     static std::map<std::string, Jogador*> jogadores;
 
     public:
     Jogador(std::string nome, std::string apelido);
     ~Jogador();
+
     void cadastra_jogador(std::string nome, std::string apelido);
     void remove_jogador(std::string apelido);
     void imprime_listagem(char opcao);
+    void le_estatisticas();
+    void atualiza_estatisticas();
+
     std::string get_nome();
     std::string get_apelido();
     int get_vitorias_r();
