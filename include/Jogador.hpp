458939c927ff18462f4b32c655c3923a3953444a
Lara Amelia <laraameliamf@gmail.com>
2025-01-03 17:15:42 -0300

    Alteração em Jogador.hpp, tornando os métodos que lidam apenas com o map da classe Jogador métodos estáticos

--- include/Jogador.hpp
+++ include/Jogador.hpp
@@ -18,15 +18,14 @@ class Jogador
     public:
     Jogador(std::string nome, std::string apelido);
     Jogador(std::string nome, std::string apelido, int vr, int dr, int vl, int dl, int vjv, int djv);
-    ~Jogador();
 
-    void cadastraJogador(std::string nome, std::string apelido);
+    static void imprimeListagem(char opcao);
+    static void leEstatisticas();
+    static void atualizaEstatisticas();
+    static Jogador* buscaJogador(std::string apelido);
+    static void apagaMap();
+    void cadastraJogador(std::string apelido);
     void removeJogador(std::string apelido);
-    void imprimeListagem(char opcao);
-    void leEstatisticas();
-    void atualizaEstatisticas();
-    Jogador* buscaJogador(std::string apelido);
-    void apagaMap();
     void registrarVitoria(std::string apelidoVencedor, char jogo);
     void registrarDerrota(std::string apelidoPerdedor, char jogo);
 
