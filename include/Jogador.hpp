cb6178a0428bd1550d92dbb45824014938f16cfe
Lara Amelia <laraameliamf@gmail.com>
2025-01-03 10:55:42 -0300

    Alteração dos nomes de variáveis e métodos na classe Jogador, passando a utilizar camelCase

--- include/Jogador.hpp
+++ include/Jogador.hpp
@@ -6,12 +6,12 @@
 class Jogador
 {
     private:
-    std::string nome_jogador;
-    std::string apelido_jogador;
+    std::string nomeJogador;
+    std::string apelidoJogador;
 
-    int vitorias_reversi, derrotas_reversi;
-    int vitorias_lig, derrotas_lig;
-    int vitorias_jvelha, derrotas_jvelha;
+    int vitoriasReversi, derrotasReversi;
+    int vitoriasLig, derrotasLig;
+    int vitoriasJVelha, derrotasJVelha;
     
     static std::map<std::string, Jogador*> jogadores;
 
@@ -20,23 +20,23 @@ class Jogador
     Jogador(std::string nome, std::string apelido, int vr, int dr, int vl, int dl, int vjv, int djv);
     ~Jogador();
 
-    void cadastra_jogador(std::string nome, std::string apelido);
-    void remove_jogador(std::string apelido);
-    void imprime_listagem(char opcao);
-    void le_estatisticas();
-    void atualiza_estatisticas();
-    Jogador* busca_jogador(std::string apelido);
-    void apaga_map();
-    void registrar_vitoria(std::string apelido_vencedor, char jogo);
-    void registrar_derrota(std::string apelido_perdedor, char jogo);
+    void cadastraJogador(std::string nome, std::string apelido);
+    void removeJogador(std::string apelido);
+    void imprimeListagem(char opcao);
+    void leEstatisticas();
+    void atualizaEstatisticas();
+    Jogador* buscaJogador(std::string apelido);
+    void apagaMap();
+    void registrarVitoria(std::string apelidoVencedor, char jogo);
+    void registrarDerrota(std::string apelidoPerdedor, char jogo);
 
-    std::string get_nome();
-    std::string get_apelido();
-    int get_vitorias_r();
-    int get_derrotas_r();
-    int get_vitorias_lig();
-    int get_derrotas_lig();
-    int get_vitorias_jv();
-    int get_derrotas_jv();
+    std::string getNome();
+    std::string getApelido();
+    int getVitoriasR();
+    int getDerrotasR();
+    int getVitoriasLig();
+    int getDerrotasLig();
+    int getVitoriasJV();
+    int getDerrotasJV();
 };
 #endif
