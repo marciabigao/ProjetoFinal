db783b8b4ad0ab89b673c61a5416d1f96de55a6d
Paulo Henrique <paulo.carmona17@gmail.com>
2025-01-03 05:19:45 -0300

    Alterações na Classe Abstrata Jogos para facilitar o entendimento

--- include/Jogos.hpp
+++ include/Jogos.hpp
@@ -6,23 +6,28 @@
 
 class Jogos {
 protected:
-    std::vector<std::vector<char>> tabuleiro;
+    std::vector<std::vector<char>> tabuleiro; 
 
 public:
+    // Construtor e Destrutor
     Jogos(int linhas, int colunas);
-    virtual void lerJogada(int& linha, int&coluna) = 0;
+    virtual ~Jogos();
+    
+    //Tabuleiro
+    const std::vector<std::vector<char>> & getTabuleiro() const;
+    void setTabuleiro(const std::vector<std::vector<char>> & novoTabuleiro);
+    virtual void imprimirTabuleiro() const;
+    void reiniciarTabuleiro();
+
+    //Celula
+    char getCelula(int linha, int coluna) const;
+    void setCelula(int linha, int coluna, char valor);
+    
+    //Metodos Virtuais 
+    virtual void lerJogada(int linha, int coluna) = 0;
     virtual bool testarValidade (int linha, int coluna) const = 0;
     virtual bool testarVitoria() const = 0;
 
-    virtual void imprimirTabuleiro() const;
-    virtual ~Jogos();
-
-    const std::vector<std::vector<char>> & getTabuleiro() const;
-    void setTabuleiro(const std::vector<std::vector<char>> & novoTabuleiro);
-    void setCelula(int linha, int coluna, char valor);
-    char getCelula(int linha, int coluna) const;
-
-    void reiniciarTabuleiro();
 };
 
 #endif
--- src/Jogos.cpp
+++ src/Jogos.cpp
@@ -4,6 +4,16 @@ Jogos::Jogos(int linhas, int colunas){
     tabuleiro.resize(linhas, std::vector<char>(colunas, ' '));
 }
 
+Jogos::~Jogos() {}
+
+const std::vector<std::vector<char>>& Jogos::getTabuleiro() const{
+    return tabuleiro;
+}
+
+void Jogos::setTabuleiro(const std::vector<std::vector<char>>& novoTabuleiro){
+    tabuleiro = novoTabuleiro;
+}
+ 
 void Jogos::imprimirTabuleiro() const{
     for(const auto& linha : tabuleiro){
         for(const auto& celula : linha){
@@ -15,20 +25,23 @@ void Jogos::imprimirTabuleiro() const{
     std::cout << std::string(tabuleiro[0].size() * 2 + 1, '-') << "\n";
 } 
 
-Jogos::~Jogos() {}
-
 void Jogos::reiniciarTabuleiro(){
     for(auto& linha : tabuleiro){
         std::fill(linha.begin(), linha.end(), ' '); //fill vai preencher a linha com ' ' (espaço em branco)
     }
 }
 
-const std::vector<std::vector<char>>& Jogos::getTabuleiro() const{
-    return tabuleiro;
-}
-
-void Jogos::setTabuleiro(const std::vector<std::vector<char>>& novoTabuleiro){
-    tabuleiro = novoTabuleiro;
+char Jogos::getCelula(int linha, int coluna) const {
+    if (linha >= 0 && linha < static_cast<int>(tabuleiro.size()) &&
+        coluna >= 0 && coluna < static_cast<int>(tabuleiro[0].size())) {
+        char valor = tabuleiro[linha][coluna];
+        
+        return valor; 
+    } 
+    else {
+        std::cerr << "Posição Inválida: Fora dos limites do tabuleiro\n";
+        return '\0'; // Caractere Nulo
+    }
 }
 
 void Jogos::setCelula(int linha, int coluna, char valor){
@@ -41,18 +54,3 @@ void Jogos::setCelula(int linha, int coluna, char valor){
     }
 }
 
-char Jogos::getCelula(int linha, int coluna) const {
-    if (linha >= 0 && linha < static_cast<int>(tabuleiro.size()) &&
-        coluna >= 0 && coluna < static_cast<int>(tabuleiro[0].size())) {
-        char valor = tabuleiro[linha][coluna];
-        if (valor != ' ') {
-            std::cerr << "Posição Inválida: Já está ocupada com '" << valor << "'\n";
-            return '\0'; // Caractere Nulo
-        }
-        return valor; 
-    } 
-        else {
-        std::cerr << "Posição Inválida: Fora dos limites do tabuleiro\n";
-        return '\0'; // Caractere Nulo
-    }
-}
