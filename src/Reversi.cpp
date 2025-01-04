7d0af4a8e7fea41698c7eae16092c2610b0e1940
Márcia Bigão <mbigaod@gmail.com>
2025-01-04 15:44:01 -0300

    testar validade jogada reversi

--- src/Reversi.cpp
+++ src/Reversi.cpp
@@ -8,3 +8,66 @@ Reversi::Reversi() : Jogos(8, 8) {
 }
 
 Reversi::~Reversi() {}
+
+bool Jogos::testarValidade(int linha, int coluna) {
+    bool validade = true;
+
+    if(this->tabuleiro[linha][coluna] != ' ')
+    {
+        validade = false;
+    }
+
+    for(int i = 1; i <= 8; i++)
+    {
+        if(i != coluna && this->tabuleiro[linha][coluna] == this->tabuleiro[linha][i])
+        {
+            int pecasOpostas = 0;
+
+            for(int j = std::min(i, coluna); j < std::max(i, coluna); j++)
+            {
+                if(this->tabuleiro[linha][j] == ' ')
+                {
+                    validade = false;
+                }
+
+                if(this->tabuleiro[linha][j] != ' ' && this->tabuleiro[linha][j] != this->tabuleiro[linha][coluna])
+                {
+                    pecasOpostas++;
+                }
+            }
+
+            if(pecasOpostas == 0)
+            {
+                validade = false;
+            }
+        }
+    }
+
+    for(int i = 1; i <= 8; i++)
+    {
+        if(i != linha && this->tabuleiro[linha][coluna] == this->tabuleiro[i][coluna])
+        {
+            int pecasOpostas = 0;
+            
+            for(int j = std::min(i, linha); j < std::max(i, linha); j++)
+            {
+                if(this->tabuleiro[j][coluna] == ' ')
+                {
+                    validade = false;
+                }
+
+                if(this->tabuleiro[j][coluna] != ' ' && this->tabuleiro[j][coluna] != this->tabuleiro[linha][coluna])
+                {
+                    pecasOpostas++;
+                }
+            }
+
+            if(pecasOpostas == 0)
+            {
+                validade = false;
+            }
+        }
+    }
+
+    return validade;
+}
