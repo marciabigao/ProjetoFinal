37de9bce19b5a659cdaa4b1415ef772fd7b0a916
Márcia Bigão <mbigaod@gmail.com>
2025-01-08 09:59:09 -0300

    criando arquivo do fluxo e estrutura básica de comandos

---
+++ src/main.cpp
@@ -0,0 +1,41 @@
+#include "../include/Jogador.hpp"
+#include "../include/Reversi.hpp"
+#include "../include/JogodaVelha.hpp"
+#include "../include/Lig4.hpp"
+#include <iostream>
+#include <string>
+
+int main() {
+
+    std::string operacao;
+
+    do
+    {
+        std::cin >> operacao;
+
+        if(operacao == "CJ")
+        {
+
+        }
+        else if(operacao == "RJ")
+        {
+
+        }
+        else if(operacao == "LJ")
+        {
+
+        }
+        else if(operacao == "EP")
+        {
+
+        }
+        else if(operacao == "FS")
+        {
+
+        }
+
+    } while (operacao != "FS");
+    
+
+    return 0;
+}
