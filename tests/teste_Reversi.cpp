0c405cd3e8f5883456619ebe4d230c1c243683e3
Paulo Henrique <paulo.carmona17@gmail.com>
2025-01-23 20:44:47 -0300

    Adição dos Testes do Reversi

--- tests/teste_Reversi.cpp
+++ tests/teste_Reversi.cpp
@@ -29,26 +29,38 @@ TEST_CASE("Testando método de validação"){
     CHECK(!teste.testarValidade(5,4));
 }
 
-TEST_CASE("Testando método que testa vitória"){
+TEST_CASE("Testando método de vitória"){
+
+    Reversi teste;
+
+    for(int i = 0; i < 8; i++){
+        for(int j = 0; j < 7; j++){
+            if(!((i == 4 && j == 3) || (i == 3 && j == 4)))
+                teste.executarJogada(i,j);
+            CHECK(!teste.testarVitoria());
+        }
+    }
+
+    for(int i = 0; i < 7; i++){
+        CHECK(!teste.testarVitoria());
+        teste.executarJogada(i,7);
+    }
+
+    teste.executarJogada(7,7);
+
+    CHECK(teste.testarVitoria());
+
+}
+
+TEST_CASE("Testando método de executar jogada"){
 
     Reversi teste;
 
     for(int i = 0; i < 8; i++){
         for(int j = 0; j < 8; j++){
-            if(!((i == 4 && j == 3) || (i == 3 && j == 4)))
-                teste.executarJogada(i,j);
-            teste.imprimirTabuleiro();
-            CHECK(!teste.testarVitoria());
+
+            teste.executarJogada(i,j);
+            CHECK(teste.getCelula(i,j));
         }
     }
-
-    teste.imprimirTabuleiro();
-    CHECK(!teste.testarVitoria());
-
-    teste.executarJogada(4, 3);
-    teste.executarJogada(3, 4);
-
-    teste.imprimirTabuleiro();
-    CHECK(teste.testarVitoria());
-
 }
